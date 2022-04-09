c6bcaff160cac01f04cfc6980c79d7f9ab59cda7
nome <102432272+ThamaraHub@users.noreply.github.com>
2022-04-09 13:58:38 -0300

    atualização arq. HelloWorld.java e criação ImprimeJava.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,16 @@ public class HelloWorld
     public static void main(String[] args) {
 
         hello();
+	 ola();
     }
 
     public static void hello()
     {
         System.out.println("Olá mundo!");
     }
+
+    public static void ola()
+    {
+        System.out.println("Hello world!");
+    }    
 }
---
+++ src/ImprimeJava.java
@@ -0,0 +1,7 @@
+public class ImprimeJava
+{
+    public static void main(String[] args) {
+
+        System.out.println("Java");
+    }
+}
